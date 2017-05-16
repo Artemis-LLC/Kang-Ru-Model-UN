6c897de045e8fc9c0579d8c2fe044ef3cfecc47e
sammayo <sammayo888@gmail.com>
2017-05-16 09:20:34 -0400

    update main smart contract

--- contracts/ModelUNMain.sol
+++ contracts/ModelUNMain.sol
@@ -1,25 +1,33 @@
 pragma solidity ^0.4.4;
 
 contract ModelUNMain {
-	struct delegate {
+	struct Delegate {
 		address id;
 		string name;
+		string phoneNumber;
+		string email;
+	}
+
+	struct Committee {
+		string name;
+
 	}
 
 	struct Conference {
 		address host;
 		string name;
 		uint cost;
-		string[] committees;
-		mapping (delegate => bool) attending;
-		mapping (delegate => uint) amountPaid;
-		mapping (delegate => string) committee;
+		Committee[] committees;
+		mapping (Delegate => bool) isFaculty;
+		mapping (Delegate => bool) attending;
+		mapping (Delegate => uint) amountPaid;
+		mapping (Delegate => Committee) committee;
 		uint balance;
 	}
 
 	Conference[] conferences;
 
-	function createConference(string name, uint cost, string[] committees)
+	function createConference(string name, uint cost) payable
 		retruns (Conference)
 	{
 		Conferece c = Conference(msg.sender, name, cost);
